a26e604a059974b0b654075760f3693d0e331620
Abdulhameed Dere <77274597+DereAbdulhameed@users.noreply.github.com>
2022-02-06 11:07:17 -0800

    Web app

--- Mental_Health_Assessment_In_Unilorin.py
+++ Mental_Health_Assessment_In_Unilorin.py
@@ -18,10 +18,10 @@ import streamlit as st
 import pickle
 from PIL import Image
 
-model = pickle.load(open('finalized_model.sav', 'rb'))
+model = pickle.load(open('final_model.sav', 'rb'))
 
-image = Image.open('index.png')
-st.image(image, '')
+#image = Image.open('index.png')
+#st.image(image, '')
 
 st.write("""
 
@@ -31,6 +31,7 @@ st.write("""
     
 st.sidebar.header('Biodata')
 
+st.subheader('Tell me about yourself, let us make a diagnosis!' )
 
 
 Year_List = [100,200,300,400,500,600]
@@ -48,15 +49,17 @@ def user_input_features():
     financial = st.sidebar.selectbox('Do you have a good support system that you can fall back on during financial crisis?', [0, 1])
     emotional = st.sidebar.selectbox('Do you have a good support system that you can fall back on during emotional crisis?', [0, 1])
     sleep = st.slider('On an average, how many hours do you sleep daily??',4,10)
-    esteem = st.slider('In a range of 1-5, how much has medical training negatively affected your self esteem?', 0,5)
+    esteema = st.slider('In a range of 1-5, how much has medical training negatively affected your self esteem?', 0,5)
+    esteemb = st.selectbox ('Do you think you self esteem has been negatively affected by medical school',[0,1],0)
     isolation = st.slider('Do you experience social isolation, withdrawal from friends and loneliness often?',0,5)
     concentration = st.slider('Do you have reduced ability to concentrate for over a short period?', 0,5)
     assessment = st.slider('How do you describe your assessment in medical school?',0,5)
     burnout = st.slider('Do you frequently experience burnout, anxiety and depression especially when exams are getting closer?', 0,5)
     history = st.selectbox('Is there a history of mental illness in a blood relative, such as a parent or sibling?',[0,1],0)
+    abuse = st.selectbox('Have you ever experienced a childhood abuse, neglect or trauma?',[0,1],0)
     extracurricular = st.selectbox('Medicine apart, do you have any extracurricular activities you engage in?',[0,1],0)
     suicide = st.selectbox('Have you ever had suicidal thoughts before?',[0,1],0)
-
+    repeat = st.selectbox('Have you ever repeated a class in medical school?',[0,1],0)
     
     
     data = {'level_class': level_class,
@@ -66,13 +69,17 @@ def user_input_features():
             'financial': financial,
             'emotional': emotional,
             'sleep': sleep,
-            'esteem': esteem,
+            'esteema': esteema,
+            'esteemb': esteemb,
             'isolation': isolation,
             'concentration': concentration,
             'assessment': assessment,
             'burnout': burnout,
+            'history': history,
+            'abuse': abuse,
             'extracurricular': extracurricular,
-            'suicide': suicide
+            'suicide': suicide,
+            'repeat': repeat
         
         }
     
@@ -81,13 +88,22 @@ def user_input_features():
 
 df = user_input_features()
     
-st.subheader('Tell me about yourself, let us make a diagnosis!' )
 
 
 
 
-st.subheader('Diagnosis')
+
+
 diagnosis = model.predict(df)
+#st.subheader('Diagnosis')
+#st.subheader(diagnosis[0])
+
+submit = st.button('Get diagnosis!')
+if submit: 
+        prediction = model.predict(df)
+        st.info(prediction[0])
+else:
+    st.error('Please Enter All the Details')
 #Note, you need to include all the features in df to be able to use the pickled nodek to predict.
 
 
